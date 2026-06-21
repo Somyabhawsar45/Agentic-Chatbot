95e7b67ea5baae564ba971abdf566434161e02bd
Somyabhawsar45 <Somyabhawsar194@gmail.com>
2026-06-21 19:45:36 +0530

    Fix Tavily tool schema mismatch by pinning topic parameter

--- src/langgraphagenticai/tools/search_tool.py
+++ src/langgraphagenticai/tools/search_tool.py
@@ -5,7 +5,7 @@ def get_tools():
     """
     Return the list of tools to be used in the chatbot
     """
-    tools=[TavilySearch(max_results=2)]
+    tools = [TavilySearch(max_results=2, topic="general")]
     return tools
 
 def create_tool_node(tools):
@@ -13,4 +13,3 @@ def create_tool_node(tools):
     creates and returns a tool node for the graph
     """
     return ToolNode(tools=tools)
-
